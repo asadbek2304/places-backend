9a0537ef8bbee39191b432ed1907a5fa5f1ea7b6
asadbek2304 <abduxowimoff@gmail.com>
2021-05-21 13:31:11 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const placesRoutes = require('./routes/places-routes')
+const app = express();
+
+app.use('/api/places',placesRoutes)
+
+app.use((err, req, res, next) => {
+    if(res.headersSent) {
+        return next(err);
+    }
+
+    res.status(err.code || 500);
+    res.json({message: err.message || "An unknown error occured"})
+})
+
+
+
+app.listen(5000)
